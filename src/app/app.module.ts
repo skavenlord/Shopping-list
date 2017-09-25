ab723eb13a4162507cb56912738042cb4a700a25
Kevin.pre <kevin.pre@gmail.com>
2017-09-25 23:45:26 +0200

    Interactions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -22,7 +24,9 @@ import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-i
     RecipeItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
