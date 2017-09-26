f236047d602c4d127bfe999b49fe2b42cce2e1cd
Kévin <kp@webpopulation.com>
2017-09-26 11:18:28 +0200

    Auto stash before merge of "master" and "origin/master"
    Auto merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
